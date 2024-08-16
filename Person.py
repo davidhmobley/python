88bb43d3689bee00a29e732f97d536bd53f56375
David Mobley <davidhmobley@gmail.com>
2024-08-16 14:03:46 -0400

    hmmmm

--- Person.py
+++ Person.py
@@ -1,10 +1,10 @@
 import House
 
 class Person:
-    House.House(House) == None
+#    House.House(House) == None
 
-    def setHouse(house):
-        self.house = house
+    def setHouse(House):
+        self.house = House
 
     def __init__(self, name, age, eyeColor):
         self.name = name
