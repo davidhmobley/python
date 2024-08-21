775263d90174d19e56bc48b0cb95c27c1a727dfb
David Mobley <davidhmobley@gmail.com>
2024-08-21 10:48:42 -0400

    show nametypes

--- main.py
+++ main.py
@@ -19,6 +19,11 @@ print("\nlast time...")
 print(p1)
 print(p2)
 
+# lists of nametypes
+fNames = []
+mNames = []
+lNames = []
+
 print("\nRead a file...")
 try:
 	f = open("./values")
@@ -26,11 +31,25 @@ try:
 		x = line.split()
 		if len(x) == 3:
 			print("FirstName: " + x[0] + " MiddleName: " + x[1] + " LastName: " + x[2])
+			fNames.append(x[0])
+			mNames.append(x[1])
+			lNames.append(x[2])
 		elif len(x) == 2:
 			print("FirstName: " + x[0] + " NoMiddleName LastName: " + x[1])
+			fNames.append(x[0])
+			lNames.append(x[1])
 		elif len(x) == 1:
 			print("FirstName: " + x[0] + " NoMiddleName NoLastName")
+			fNames.append(x[0])
 except:
 	print("Exception!!!")
 finally:
 	f.close()
+
+# show nametypes
+print("\nFirst Names...")
+print( fNames)
+print("Middle Names...")
+print( mNames)
+print("Last Names...")
+print(lNames)
